cfbbd4062c34967d8153103f0e18d4575408a0b4
Rigers Leka <rigers.leka@sisal.al>
2022-10-12 16:36:12 +0200

    implement context logic

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,67 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+
+/* Import Context */
+import githubContext from "./githubContext";
+import githubReducer from "./githubReducer";
+import { SEARCH_USERS, SET_LOADING, GET_USER } from "../types";
+
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== "production") {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
+const GithubState = (props) => {
+  const initialState = {
+    users: [],
+    user: {},
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  // Search User
+  const searchUsers = async (text) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items,
+    });
+  };
+
+  // Get USer
+  const getUser = async (userName) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${userName}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
+    );
+    dispatch({ type: GET_USER, payload: res.data });
+  };
+
+  // Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
+  return (
+    <githubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        searchUsers,
+        getUser,
+      }}
+    >
+      {props.children}
+    </githubContext.Provider>
+  );
+};
+
+export default GithubState;
---
+++ src/context/github/types.js
@@ -0,0 +1,3 @@
+export const SEARCH_USERS = 'SEARCH_USERS';
+export const GET_USER = 'GET_USER';
+export const SET_LOADING = 'SET_LOADING';
